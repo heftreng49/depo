85d2ffaf853dbe63e88cd91f0ef9bf4b8bc825b2
heftreng49 <65556332+heftreng49@users.noreply.github.com>
2020-06-11 13:59:27 +0300

    Update galeri.js

--- galeri.js
+++ galeri.js
@@ -1,13 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// fetch('https://api.unsplash.com/photos/random/?count=5&client_id=52d8369eb3e2576a5f5b6423865e074e9c7045761bff1ac5664ff3e0bdb57a1d') 
-//   .then(response => response.json())
-//   .then(data => {
-//     data.forEaach(function(image, i) {
-//       document.querySelector("#slide-" + (i+1)).innerHTML = `
-//         <img src="${image.urls.regular}" alt="">
-//         <p class="author-info">
-//           <a href="${image.links.html}?utm_source=slider-thing&utm_medium=referral&utm_campaign=api-credit">Photo by ${image.user.name}</a> on <a href="https://unsplash.com/">Unsplash</a>
-//         </p>
-//       `;
-//     });
-//   });
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
